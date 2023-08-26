b20f519107bb89a068c23aa0428df94f5e8a53a7
Sarah Quinney <quinney.sarah@gmail.com>
2023-08-26 17:30:00 -0500

    homepage styled

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -13,10 +13,18 @@ export default function HomePage() {
             </header>
             <main>
                 <div className="ring-container">
-                    <img id="ring-img" src={ring} alt="yellow diamond ring"/>
+                    <img id="ring-img" src={ring} alt="yellow diamond ring" />
+                </div>
+                <div className="welcome-container">
+                    <div id="welcome-title">
+                        WELCOME TO OUR STORE
+                    </div>
+                    <div id="welcome-info">
+                        Unique Design has been located in Jewerly City for 50 years. This long-lived, family owned business has been a staple of the community for many years. Unique Design's staff has over 100 years of combined jewelry experience. You can trust that you will leave Unique Design with the highest quality future family heirloom money can buy. Our store offers a full service jewelery shop, appraisals for insurance and resale purposes, custom jewerly design support, and a beautiful showroom display of custom jewerly to meet all of your jewelry needs.
+                    </div>
                 </div>
             </main>
-           
+
         </div>
     )
 }
