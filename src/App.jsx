25854d966fa21b831644a36ef4d633a2bdfe2f01
Sarah Quinney <quinney.sarah@gmail.com>
2023-08-28 22:50:32 -0500

    moved header to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,38 @@ import Appraisals from './Appraisals';
 import ShopPage from './ShopPage';
 import Cart from './Cart';
 import FormPage from './FormPage';
+import { useState } from "react";
+import { useLocation } from 'react-router-dom'
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
+  
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([])
+  const { pathname } = useLocation();
+  const [totalValue, setTotalValue] = useState(0);
+  const [val1, setVal1] = useState(0);
+  const [val2, setVal2] = useState(0);
+  const [val3, setVal3] = useState(0);
+  const [showCart, setShowCart] = useState(false)
 
   return (
-    
+    <div>
+      <Header />
       <Routes>
-        <Route path="*" element={ <HomePage /> } />
-        <Route path="formpage" element={ <FormPage /> } />
-        <Route path="about" element={ <About /> } />
-        <Route path="team" element={ <Team /> } />
-        <Route path="services" element={ <Services /> } />
-        <Route path="appraisals" element={ <Appraisals /> } />
-        <Route path="shoppage" element={ <ShopPage /> } />
-        <Route path="cart" element={ <Cart /> } />
+        <Route path="*" element={<HomePage />} />
+        <Route path="formpage" element={<FormPage />} />
+        <Route path="about" element={<About />} />
+        <Route path="team" element={<Team />} />
+        <Route path="services" element={<Services />} />
+        <Route path="appraisals" element={<Appraisals />} />
+        <Route path="shoppage" element={<ShopPage />} />
+        <Route path="cart" element={<Cart />} />
       </Routes>
-    
+      <Footer />
+    </div>
   )
 }
 
